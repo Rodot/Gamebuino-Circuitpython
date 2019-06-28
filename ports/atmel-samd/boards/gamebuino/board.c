a895f25715eea5eac12e18733c24fd73582557c1
Sorunome <mail@sorunome.de>
2019-06-28 17:11:22 +0200

    add adalie license header

--- ports/atmel-samd/boards/gamebuino/board.c
+++ ports/atmel-samd/boards/gamebuino/board.c
@@ -3,7 +3,9 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) Aadalie 2019
+ * Authors:
+ *  - Sorunome
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- py/circuitpy_defns.mk
+++ py/circuitpy_defns.mk
@@ -3,6 +3,7 @@
 # The MIT License (MIT)
 #
 # Copyright (c) 2019 Dan Halbert for Adafruit Industries
+# Copyright (c) 2019 Aadalie - adding C++ support by Sorunome
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
